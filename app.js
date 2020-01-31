1b84e88eafd00eeaf7b40338079ee9010d57a42f
dlentz92 <dlentz92@gmail.com>
2020-01-31 15:11:36 -0500

    ids set up

---
+++ app.js
@@ -0,0 +1,7 @@
+// DEFINE UI VARIABLES
+
+const form = document.getElementById("task-form");
+const taskList = document.getElementsByClassName("collection");
+const clearBtn = document.getElementsByClassName("clear-tasks");
+const filter = document.getElementById("filter");
+const taskInput = document.getElementById("task");
